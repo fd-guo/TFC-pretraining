28caac6e6b3f7efc48e482b9cb04fa759f09f1d1
Fengdi Guo <fguo@cmtelematics.com>
2024-03-12 19:51:18 +0000

    feat: use gpt2 for base model [PDS-512]

--- code/TFC/model.py
+++ code/TFC/model.py
@@ -1,6 +1,10 @@
 from torch import nn
 import torch
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from transformers.models.gpt2.modeling_gpt2 import GPT2Model
+from embed import DataEmbedding
+import torch.nn.functional as F
+from einops import rearrange
 
 """Two contrastive encoders"""
 class TFC(nn.Module):
@@ -29,12 +33,14 @@ class TFC(nn.Module):
 
 
     def forward(self, x_in_t, x_in_f):
+        # x_in_t: (batch_size, feat_dim, max_seq_len)
+        # x_in_f: (batch_size, feat_dim, max_seq_len)
         """Use Transformer"""
-        x = self.transformer_encoder_t(x_in_t)
-        h_time = x.reshape(x.shape[0], -1)
+        x = self.transformer_encoder_t(x_in_t)  # (batch_size, feat_dim, max_seq_len)
+        h_time = x.reshape(x.shape[0], -1)      # (batch_size, feat_dim * max_seq_len)
 
         """Cross-space projector"""
-        z_time = self.projector_t(h_time)
+        z_time = self.projector_t(h_time)       # (batch_size, embedding)
 
         """Frequency-based contrastive encoder"""
         f = self.transformer_encoder_f(x_in_f)
@@ -58,3 +64,78 @@ class target_classifier(nn.Module):
         emb = torch.sigmoid(self.logits(emb_flat))
         pred = self.logits_simple(emb)
         return pred
+
+
+"""GPT based transformers"""
+class gpt4ts(nn.Module):
+    
+    def __init__(self, configs):
+        super(gpt4ts, self).__init__()
+        self.pred_len = 0
+        self.seq_len = configs.max_seq_len
+        self.patch_size = configs.patch_size
+        self.stride = configs.stride
+        self.gpt_layers = configs.gpt_layers
+        self.feat_dim = configs.feat_dim
+        self.d_model = configs.d_model
+
+        self.patch_num = (self.seq_len - self.patch_size) // self.stride + 1
+        self.patch_num += 1
+
+        # time domain transformer 
+        self.padding_patch_layer_t = nn.ReplicationPad1d((0, self.stride)) 
+        self.enc_embedding_t = DataEmbedding(self.feat_dim * self.patch_size, configs.d_model, configs.dropout)
+        self.gpt2_t = GPT2Model.from_pretrained('gpt2', output_attentions=True, output_hidden_states=True)
+        # gpt2 has 12 layers 
+        self.gpt2_t.h = self.gpt2_t.h[:self.gpt_layers]
+        for i, (name, param) in enumerate(self.gpt2_t.named_parameters()):
+            if 'ln' in name or 'wpe' in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+        self.act = F.gelu
+        self.dropout_t = nn.Dropout(0.1)
+        self.ln_proj_t = nn.LayerNorm(configs.d_model * self.patch_num)
+        self.out_layer_t = nn.Linear(configs.d_model * self.patch_num, configs.embedding_len)
+
+        # frequency domain transfomer 
+        self.padding_patch_layer_f = nn.ReplicationPad1d((0, self.stride)) 
+        self.enc_embedding_f = DataEmbedding(self.feat_dim * self.patch_size, configs.d_model, configs.dropout)
+        self.gpt2_f = GPT2Model.from_pretrained('gpt2', output_attentions=True, output_hidden_states=True)
+        self.gpt2_f.h = self.gpt2_f.h[:self.gpt_layers]
+        for i, (name, param) in enumerate(self.gpt2_f.named_parameters()):
+            if 'ln' in name or 'wpe' in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+        self.act = F.gelu
+        self.dropout_f = nn.Dropout(0.1)
+        self.ln_proj_f = nn.LayerNorm(configs.d_model * self.patch_num)
+        self.out_layer_f = nn.Linear(configs.d_model * self.patch_num, configs.embedding_len)
+        
+    def forward(self, x_in_t, x_in_f):
+        # time domain 
+        B_t, M_t, L_t = x_in_t.shape # B: batch_size, M: feat_dim, L: max_seq_len 
+        input_t = self.padding_patch_layer_t(x_in_t)
+        input_t = input_t.unfold(dimension=-1, size=self.patch_size, step=self.stride) # (B, M, new_len, patch_size)
+        input_t = rearrange(input_t, 'b m n p -> b n (p m)') # (B, new_len, new_fea)
+        outputs_t = self.enc_embedding_t(input_t, None) # (B, new_len, d_model)
+        outputs_t = self.gpt2_t(inputs_embeds=outputs_t).last_hidden_state  # (B, new_len, d_model)
+        h_time = outputs_t.reshape(outputs_t.shape[0], -1) # (B, new_len*d_model)
+        outputs_t = self.act(outputs_t).reshape(B_t, -1) # (B, new_len*d_model)
+        outputs_t = self.ln_proj_t(outputs_t) # (B, new_len*d_model)
+        z_time = self.out_layer_t(outputs_t) # (B, embedding_len)
+        
+        # freq domain 
+        B_f, M_f, L_f = x_in_f.shape # B: batch_size, M: feat_dim, L: max_seq_len 
+        input_f = self.padding_patch_layer_f(x_in_f)
+        input_f = input_f.unfold(dimension=-1, size=self.patch_size, step=self.stride) # (B, M, new_len, patch_size)
+        input_f = rearrange(input_f, 'b m n p -> b n (p m)') # (B, new_len, new_fea)
+        outputs_f = self.enc_embedding_f(input_f, None) # (B, new_len, d_model)
+        outputs_f = self.gpt2_f(inputs_embeds=outputs_f).last_hidden_state  # (B, new_len, d_model)
+        h_freq = outputs_f.reshape(outputs_f.shape[0], -1) # (B, new_len*d_model)
+        outputs_f = self.act(outputs_f).reshape(B_f, -1) # (B, new_len*d_model)
+        outputs_f = self.ln_proj_f(outputs_f) # (B, new_len*d_model)
+        z_freq = self.out_layer_f(outputs_f) # (B, embedding_len)
+        
+        return h_time, z_time, h_freq, z_freq 
--- code/config_files/MotionSample_Configs.py
+++ code/config_files/MotionSample_Configs.py
@@ -1,23 +1,30 @@
 class Config(object):
     def __init__(self):
-        # model configs
+        # model configs for TFC 
         self.input_channels = 8  # feature count
         self.transformer_nhead = 2
         self.transformer_num_layers = 2
-        self.embedding_len = 160  # final embedding len = embedding_len*2
-
-        # training configs
-        self.num_epoch = 2
-        
-        # optimizer parameters
+        self.TSlength_aligned = 900  # sequence length 15Hz * 60second
         self.beta1 = 0.9
         self.beta2 = 0.99
         self.lr = 3e-4  # original lr: 3e-4
 
-        # data parameters
-        self.drop_last = True
+        # model configs for gpt4ts 
+        self.patch_size = 32 
+        self.stride = 16 
+        self.d_model = 768  # this is determined by gpt model. 
+        self.dropout = 0.1
+        self.gpt_layers = 2
+        self.feat_dim = 8  # feature count
+        self.max_seq_len = 900  # seq lenth 
+        self.optimizer = "RAdam"
+        self.gpt_lr = 0.001
+
+        # common configs 
+        self.embedding_len = 160  # final embedding len = embedding_len*2
+        self.num_epoch = 2
         self.batch_size = 2 
-        self.TSlength_aligned = 900  # sequence length 15Hz * 60second
+        self.drop_last = True
         
         self.Context_Cont = Context_Cont_configs()
         self.TC = TC()
@@ -26,8 +33,8 @@ class Config(object):
 
 class augmentations(object):
     def __init__(self):
-        self.jitter_scale_ratio = 0.001
-        self.jitter_ratio = 0.001
+        self.jitter_scale_ratio = 1.1
+        self.jitter_ratio = 0.8
         self.max_seg = 5
 
 
